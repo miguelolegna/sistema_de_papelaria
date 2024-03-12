1d84f00cf5d00efa03aa51761fbc5669e2193db2
miguelolegna <miguel.olegna1@gmail.com>
2024-03-12 16:42:38 +0000

    classes produtos e objetos

--- gestao.cpp
+++ gestao.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+class produtos {       // The class
+  public:             // Access specifier
+    int quantidade;
+    double preco;
+};
 
-    return 0;
+int main() {
+  produtos lapis;  // Create an object of MyClass
+
+  // Access attributes and set values
+  lapis.quantidade = 15;
+  lapis.preco = 0.50;
+
+  // Print values
+  cout << "quantidade comprada: " << lapis.quantidade << "\n"; 
+  cout << "preco individual: " << lapis.preco << "\n"; 
+   cout << "preço total: " << lapis.quantidade * lapis.preco;
+
+    if ()
+
+
+  return 0;
 }
