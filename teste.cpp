81f68ec14aa470b6d815d4f83b8057f675d7bdf6
miguelolegna <miguel.olegna1@gmail.com>
2024-03-12 15:45:56 +0000

    valores de produtos e quantidades

---
+++ teste.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+
+ std::map<std::string, int> carValues;
+
+    // Associando valores numéricos aos carros
+    productquantity["lapis"] = 1;
+    productquantity["BMW"] = 2;
+    productquantity["Ford"] = 3;
+    productquantity["Mazda"] = 4;
+
+ if
+    carValues["lapis"] = 5;
+
+    // Imprimindo os valores associados
+    std::cout << "Valor do lapis: " << carValues["lapis"] << "\n";
+    std::cout << "Valor do BMW: " << carValues["BMW"] << "\n";
+    std::cout << "Valor do Ford: " << carValues["Ford"] << "\n";
+    std::cout << "Valor do Mazda: " << carValues["Mazda"] << "\n";
+
+
+// cout << produtos.cars[1] << "\n";
+// cout << produtos.produto1 << "\n";
+//     cout << produtos.produto2 << "\n";
+
+  return 0;
+}
